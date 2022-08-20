94e54422119603380379407b946ebdc9bf140670
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-20 12:37:07 +0500

    типы данных

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-    echo "Комментарий"; // комментарий в одну строчку
-    /*
-    Большой Комментарий
-    */
-    echo ''
+    $a_bool = TRUE;
+    $a_str = "foo";
+    $a_str2 = "foo";
+    $an_int = 12;
+    
+    echo gettype($a_bool); // boolean
+    echo gettype($a_str); // string
+    if (is_int($an_int)) {
+        $an_int += 4;
+    }
+
+    if (is_string($a_bool)) {
+        echo "Строка: $a_bool";
+    }
 ?>
-<?php # echo "простой"?>
