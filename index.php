e298cd50d28ad6eebf5049d57da7e9d82f0d723c
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-30 17:54:45 +0500

    extends final .= parent::__construct parent::info protected

--- index.php
+++ index.php
@@ -11,7 +11,15 @@
 
 class User {
     private static $name;
+    function __construct($surname, $password)
+    {
+        $this->surname = $surname;
+        $this->password = $password;
+    }
 
+    function info(){
+        return "{$this->surname} {$this->password}";
+    }
     public static function setName($name1) {
         self::$name = $name1;
     }
@@ -21,8 +29,29 @@ class User {
 }
 User::setName('Ivan');
 echo User::getName();
-User::setName('Vlad');
-echo User::getName();
+
+class Moderator extends User {
+    function __construct($id)
+    {
+        // вызвать конструктор родителя
+        parent::__construct('volod', 'qwerty');
+        $this->id = $id;
+    }
+
+    function info()
+    {
+        $st = parent::info();
+        $st .= "{$this->id}";
+        return $st;
+    }
+
+    function hello() {
+        echo "Moderator is here";
+    }
+}
+Moderator::setName('Vlad1');
+$mod = new Moderator("1234");
+echo $mod->info();
 ?>
 </body>
 </html>
