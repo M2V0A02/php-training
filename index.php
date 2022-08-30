1399c5d015ea242bf72ee6fc9f623819f8857a6a
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-30 16:37:22 +0500

    this и self

--- index.php
+++ index.php
@@ -10,69 +10,19 @@
 <?php
 
 class User {
-    // Установление значений по умолчанию
-    public $name;
-    public $password;
-    public $email;
-    public $city;
+    private static $name;
 
-
-    function __construct($name, $password, $email, $city)
-    {
-        $this->name = $name;
-        $this->password = $password;
-        $this->email = $email;
-        $this->city = $city;
+    public static function setName($name1) {
+        self::$name = $name1;
     }
-
-    function __destruct()
-    {
-        print "Уничтожение";
-    }
-
-    public function hello() {
-        return 'Hello' . $this->name;
-    }
-
-    function getInfo() {
-        return "{$this->name} {$this->email}"; 
+    public static function getName() {
+        return self::$name;
     }
 }
-
-$admin = new User('Alex', '12345', '123@mail.ru', 'kiev');
-echo "{$admin->getInfo()}";
-// $admin->Hello();
-// echo $admin->name;
-
-// $user1 = new User();
-// $admin->name = "admin";
-// $user1->name = "Andrey";
-
-// echo $admin->name;
-// echo $user1->name;
-
-// использование несуществующего свойства
-// $user1->surname = "Ivanov";
-// echo $user1->surname;
-
-// class ShoppingCard {
-//     // При наследование наследуемый класс может использовать только public и protected.
-//     private $name;
-//     public function naming() {
-//         $this->name = "Adidas";
-//         echo $this->name;
-//     }
-// };
-
-// $product1 = new ShoppingCard();
-// $product2 = new ShoppingCard();
-// $product3 = new ShoppingCard();
-
-// $product1->naming();
-// var_dump($product1);
-// var_dump($product2);
-// var_dump($product1 instanceof ShoppingCard);
-// var_dump($product2 instanceof Attribute);
+User::setName('Ivan');
+echo User::getName();
+User::setName('Vlad');
+echo User::getName();
 ?>
 </body>
 </html>
