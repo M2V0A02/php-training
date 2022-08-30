ac22e69d49b5165e87960312e06ba09e2c1b867f
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-30 15:54:18 +0500

    Добавление свойств с разным доступом вывод свойств и изменение

--- index.php
+++ index.php
@@ -8,14 +8,43 @@
 </head>
 <body>
 <?php
-Class ShoppingCard {
 
+class User {
+    // Установление значений по умолчанию
+    public $name = "Egor";
+    public $password = "qwerty";
+    public $email = "123@tand.ru";
+    public $city = "Город";
+}
+
+$admin = new User();
+echo $admin->name;
+
+$user1 = new User();
+$admin->name = "admin";
+$user1->name = "Andrey";
+
+echo $admin->name;
+echo $user1->name;
+
+// использование несуществующего свойства
+$user1->surname = "Ivanov";
+echo $user1->surname;
+
+class ShoppingCard {
+    // При наследование наследуемый класс может использовать только public и protected.
+    private $name;
+    public function naming() {
+        $this->name = "Adidas";
+        echo $this->name;
+    }
 };
 
 $product1 = new ShoppingCard();
 $product2 = new ShoppingCard();
 $product3 = new ShoppingCard();
 
+$product1->naming();
 var_dump($product1);
 var_dump($product2);
 var_dump($product1 instanceof ShoppingCard);
