c373611ac1fef61c06e83b0cefedb3b1e4ec6121
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-31 11:42:27 +0500

    рекурсия

--- index.php
+++ index.php
@@ -8,17 +8,16 @@
 </head>
 <body>
 <?php
-    $my_array = ['h', 'e', 'l', 'l', 'o'];
-    $my_array = array_reverse($my_array);
-    print_r($my_array);
-    $reversed = array();
-    for ($i = 0; $i < count($my_array); $i++) array_unshift($reversed, $my_array[$i]);
-    print_r($reversed);
-    $str = "Turn Str";
-    print($str);
-    $str1 = '';
-    for($i = strlen($str) - 1; $i >= 0; $i--) $str1 .= $str[$i];
-    print($str1);
+    function fac($x)
+    {
+        if ($x === 0){
+            return 1;
+        }
+        else {
+            return $x * fac($x - 1);
+        }
+    }
+    echo fac(5);
 ?>
 </body>
 </html>
